4f92a163b573dddad265ff17638a0557c621e232
Devon Fulcher <24593113+DevonFulcher@users.noreply.github.com>
2026-01-09 10:08:46 -0600

    Using client session for client tool (#505)
    ## Summary
    
    This PR makes the `task client` tool behave more like other clients by
    opening an MCP session. This change makes `task client` more realistic,
    and it helped me test the change mentioned
    [here](https://github.com/dbt-labs/dbt-mcp/issues/498).
    
    ## Checklist
    - [x] I have performed a self-review of my code
    - [x] I have made corresponding changes to the documentation (in
    https://github.com/dbt-labs/docs.getdbt.com) if required -- Mention it
    here
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes

--- src/client/tools.py
+++ src/client/tools.py
@@ -1,3 +1,4 @@
+from mcp.types import Tool
 from openai.types.responses import (
     FunctionToolParam,
 )
@@ -5,8 +6,7 @@ from openai.types.responses import (
 from dbt_mcp.mcp.server import DbtMCP
 
 
-async def get_tools(dbt_mcp: DbtMCP) -> list[FunctionToolParam]:
-    mcp_tools = await dbt_mcp.list_tools()
+def map_tools(mcp_tools: list[Tool]) -> list[FunctionToolParam]:
     return [
         FunctionToolParam(
             type="function",
@@ -17,3 +17,8 @@ async def get_tools(dbt_mcp: DbtMCP) -> list[FunctionToolParam]:
         )
         for t in mcp_tools
     ]
+
+
+async def get_tools(dbt_mcp: DbtMCP) -> list[FunctionToolParam]:
+    mcp_tools = await dbt_mcp.list_tools()
+    return map_tools(mcp_tools)
